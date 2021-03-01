5d2da7501ec2ec9e50b686c297a9510e10114686
AntonK <Erusalim12@gmail.com>
2021-03-01 09:35:08 +0300

    Добавьте файлы проекта.

---
+++ Application.Dal/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Dal
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
