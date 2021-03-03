054f3063da56852fa47bf117ebb31c58605babcc
AntonK <Erusalim12@gmail.com>
2021-03-03 17:00:33 +0300

    fix menu and routing

--- MainSite/Startup.cs
+++ MainSite/Startup.cs
@@ -59,7 +59,7 @@ namespace MainSite
             {
 
                 endpoints.MapControllerRoute(name: "default",
-                    pattern: "{controller?=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
             });
