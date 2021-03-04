63f379b563651534b355d778ac7e8ab92cf8b3ce
roustr <artem-_96@mail.ru>
2021-03-04 12:02:13 +0300

    add front items and node-moduls

---
+++ MainSite/gulpfile.js
@@ -0,0 +1,26 @@
+﻿//const gulp = require("gulp");
+//const sass = require("gulp-sass");
+//const sourcemaps = require("sourcemaps");
+//const watch = require("gulp-watch");
+
+/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    sass = require("gulp-sass"); // добавляем модуль sass
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+// регистрируем задачу для конвертации файла scss в css
+gulp.task("sass-lib", function () {
+    return gulp.src(paths.webroot +'css/materialize/sass/materialize.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + 'css/materialize/sass'));
+});
+
+gulp.task("sass-site", function () {
+    return gulp.src(paths.webroot + 'css/site.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + 'css/'));
+});
---
+++ MainSite/wwwroot/css/menu/js/eventClickElMenu.js
@@ -0,0 +1,44 @@
+﻿
+function eventClickElementMenu() {
+    //выбираем нужные элементы
+    var a = document.querySelectorAll('.menu a');
+    
+    //перебираем все найденные элементы и вешаем на них события
+    [].forEach.call(a, function (el) {
+        //вешаем событие
+        el.onclick = function(e) {
+            var checkFirstNode = el.classList.contains('active');//el.parentElement.parentElement.classList.contains('menu');
+            if(!checkFirstNode) {
+                var activeElementsFirstChildrensMainNode = el.parentElement.querySelectorAll('*.active');
+                if(activeElementsFirstChildrensMainNode.length > 0) {
+                    activeElementsFirstChildrensMainNode.forEach(function (element) { element.classList.remove('active'); });
+                    return null;
+                }
+            }
+            document.querySelectorAll('.menu ul, .menu a, .menu li').forEach(function (elem) { elem.classList.remove('active'); });
+            !el.classList.contains('active') ?SearchParent(el, '.menu'):  SearchParent(el, 'li');
+        }
+    });
+}
+
+function SearchParent(node, searchParent) {    
+    if(node.parentElement.children.length == 1) node.classList.add('active');
+    while (node) {
+        if (node.matches(searchParent))
+        { 
+            return node; 
+        }
+        else {
+            node = node.parentElement;
+            for (var i = 0; i < node.children.length; i++) {
+                if (node.children[i].nodeName.toLowerCase() == "ul") {
+                  node.children[i].classList.add('active'); 
+                  break;
+                }        
+            }
+        }
+    }
+    return null;
+}
+
+eventClickElementMenu();
