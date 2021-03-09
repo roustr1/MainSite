de8d74c986f86f2fef2dfffa4c86f6a37935418a
wisamasd <skay-91@mail.ru>
2021-03-09 15:42:45 +0300

    Добавил тест для проверки что сервис корректно обрабатвыает невалидный id при запросе файла

---
+++ UnitTests/NewsServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Dal;
+using Application.Dal.Domain.News;
+using Application.Services.News;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class NewsServiceTests
+    {
+        [TestCase("")]
+        [TestCase("00000000-0000-0000-0000-000000000000")]
+        public void GetNewsItem_RequestWithInvalidId_ReturnNull(string id)
+        {
+            Mock<IRepository<NewsItem>> mock = new Mock<IRepository<NewsItem>>();
+
+            NewsService newsService = new NewsService(mock.Object);
+            NewsItem result = newsService.GetNewsItem(id);
+
+            Assert.IsTrue(result == null);
+        }
+    }
+}
