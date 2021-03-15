7b6ab73cd4a28c94cd11e23013edff17e78c1e1a
wisamasd <skay-91@mail.ru>
2021-03-15 09:56:13 +0300

    Удалил костыль с тестовыми данными в коде

--- Application.Services/News/INewsService.cs
+++ Application.Services/News/INewsService.cs
@@ -9,9 +9,7 @@ namespace Application.Services.News
         void CreateNews(NewsItem item);
         void UpdateNews(NewsItem item);
         void DeleteNews(NewsItem item);
-        List<NewsItem> GetAllNews();
         NewsItem GetNewsItem(string itemId);
-
         IEnumerable<NewsItem> GetNewsItem(string authorId = null, string category = null,
             DateTime? startDate = null, DateTime? endDate = null, bool isNewest = true);
     }
