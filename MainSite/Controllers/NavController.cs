7227970d18018a5ed6f62917cefc8a40ba918276
wisamasd <skay-91@mail.ru>
2021-03-16 11:56:49 +0300

    Добавил тест для NavController, проверяю как создается одноранговое меню. Добавил Контроллер и модель представления для MenuItem

---
+++ MainSite/Controllers/NavController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using MainSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Linq;
+using Application.Dal;
+using Application.Dal.Domain.Menu;
+using Application.Dal.Domain.News;
+using Application.Services.Files;
+using Application.Services.Menu;
+using Application.Services.News;
+using Application.Services.Settings;
+using MainSite.Models.Common;
+using MainSite.Models.News;
+using MainSite.Models.UI.Menu;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainSite.Controllers
+{
+    public class NavController : Controller
+    {
+        private IMenuService _service;
+        public NavController(IMenuService service)
+        {
+            _service = service;
+        }
+
+        public PartialViewResult Menu(string categoryId = null)
+        {
+            ViewBag.SelectedCategory = categoryId;
+
+            var menuItems = new List<MenuItemViewModel>();
+
+            return PartialView("Menu", menuItems);
+        }
+    }
+}
---
+++ UnitTests/NavControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Application.Dal.Domain.Menu;
+using Application.Services.Menu;
+using MainSite.Controllers;
+using MainSite.Models.UI.Menu;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class NavControllerTests
+    {
+        [Test]
+        public void Menu_TestOneRangeMenu_GetFlatMenu()
+        {
+            var listObjects = new List<MenuItem>()
+            {
+                new MenuItem()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    ParentId = null,
+                },
+                new MenuItem()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    ParentId = null,
+                },
+                new MenuItem()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    ParentId = null,
+                },
+            };
+            Mock<IMenuService> mock = new Mock<IMenuService>();
+            mock.Setup(m => m.GetMenuItem()).Returns(listObjects);
+            NavController navController = new NavController(mock.Object);
+
+            IEnumerable<MenuItemViewModel> result = (IEnumerable<MenuItemViewModel>) navController.Menu().Model;
+
+            Assert.IsTrue(result.Count() == listObjects.Count);
+        }
+    }
+}
