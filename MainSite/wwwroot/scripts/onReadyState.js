84c94b7d44882c6c79527af6d86c614ca90d2cfb
roustr <artem-_96@mail.ru>
2021-03-17 11:03:25 +0300

    сортировка макетных картинок по папкам. Корректировка выделения пунктов меню

--- MainSite/wwwroot/scripts/onReadyState.js
+++ MainSite/wwwroot/scripts/onReadyState.js
@@ -15,7 +15,7 @@ function aligmentWidthChildsBirthdayBlock() {
     }
     else {
         for (var item in items) {
-            items.style.paddingRight = "70px";
+            item.style.paddingRight = "70px";
         }
     }
 }
