445c4a8e72a1da1de30606cae60704406ea1ec6b
wisamasd <skay-91@mail.ru>
2021-03-29 11:38:28 +0300

    Расширил интерфейс сервиса меню

--- Application.Services/Menu/MenuService.cs
+++ Application.Services/Menu/MenuService.cs
@@ -28,9 +28,13 @@ namespace Application.Services.Menu
                 mi.ActionName = new TranslitMethods.Translitter().Translit(mi.Name, TranslitMethods.TranslitType.Gost)
                     .Replace(' ', '_');
 
+            var maxIndexForCurrentDirectory = GetMaximumIndexForCategory(mi.ParentId);
+
+            mi.Index = maxIndexForCurrentDirectory + 1;
+
             _repository.Add(mi);
         }
-
+        
         public void DeleteItem(MenuItem mi)
         {
             if (mi == null) throw new NullReferenceException("menu item is null");
@@ -42,6 +46,13 @@ namespace Application.Services.Menu
             }
 
             _repository.Delete(mi);
+        }
+
+        public void UpdateItem(MenuItem mi)
+        {
+            if (mi == null) throw new NullReferenceException("menu item is null");
+
+            _repository.Update(mi);
         }
 
         public IEnumerable<MenuItem> GetAll()
@@ -75,6 +86,23 @@ namespace Application.Services.Menu
             }
 
             result.AddRange(tres);
+
+            return result;
+        }
+
+        private int GetMaximumIndexForCategory(string categoryId)
+        {
+            var result = 0;
+
+            var menuItems = GetManyByParentId(categoryId).ToList();
+            if (menuItems.Any())
+            {
+                var menuItem = menuItems.OrderBy(m => m.Index).LastOrDefault();
+                if (menuItem != null)
+                {
+                    result = menuItem.Index;
+                }
+            }
 
             return result;
         }
