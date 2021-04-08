29163973b0f457ad64b7710a7130c3bc5c6c700a
wisamasd <skay-91@mail.ru>
2021-04-08 11:06:09 +0300

    Добавил в модель данных таблицу для хранения информации о закрепленных сообщениях в категории

---
+++ Application.Dal/Domain/News/PinNews.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dal.Domain.News
+{
+    public class PinNews : BaseEntity
+    {
+        public string CategoryId { get; set; }
+        public string NewsItemId { get; set; }
+        public int Index { get; set; }
+    }
+}
